3f863e3d47f4366dc2f3f3958d99ddbe792ce9fc
Pranshu <malhopra001@gmail.com>
2024-10-16 12:11:57 +0530

    tried modifying mainpage to take care of habit tracking, it's getting added to the respective dates in this branch but some css is off

--- frontend/src/MainPage.js
+++ frontend/src/MainPage.js
@@ -7,8 +7,6 @@ const MainPage = ({ token, isLoggedIn }) => {
   const [habits, setHabits] = useState([]);
   const [newHabit, setNewHabit] = useState('');
   const [frequencyDays, setFrequencyDays] = useState([]);
-  const [showCalendarView, setShowCalendarView] = useState(false);
-  const [habitColor, setHabitColor] = useState('#4db6ac'); // Default color
   const [showAddHabitPopup, setShowAddHabitPopup] = useState(false);
   const navigate = useNavigate();
 
@@ -38,7 +36,7 @@ const MainPage = ({ token, isLoggedIn }) => {
 
   const handleAddHabit = async () => {
     if (newHabit && frequencyDays.length > 0) {
-      const habit = { name: newHabit, frequencyDays, color: habitColor };
+      const habit = { name: newHabit, frequencyDays };
       try {
         const response = await fetch('http://localhost:2000/habits', {
           method: 'POST',
@@ -61,33 +59,10 @@ const MainPage = ({ token, isLoggedIn }) => {
     }
   };
 
-  const handleCompleteHabit = async (habitId) => {
-    try {
-      const date = selectedDate.toISOString().split('T')[0]; // Format date as YYYY-MM-DD
-      const response = await fetch(`http://localhost:2000/habits/${habitId}/complete`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${token}`,
-        },
-        body: JSON.stringify({ date }),
-      });
-      if (response.ok) {
-        setHabits(prev => prev.map(habit => habit._id === habitId ? { ...habit, completedDates: [...habit.completedDates, date] } : habit));
-      } else {
-        const data = await response.json();
-        console.error('Failed to complete habit:', data.error);
-      }
-    } catch (error) {
-      console.error('Failed to complete habit:', error);
-    }
-  };
-
   const resetHabitForm = () => {
     setNewHabit('');
     setFrequencyDays([]);
-    setHabitColor('#4db6ac'); // Reset to default color
-    setShowAddHabitPopup(false); // Close popup
+    setShowAddHabitPopup(false);
   };
 
   const handleFrequencyChange = (day) => {
@@ -98,91 +73,64 @@ const MainPage = ({ token, isLoggedIn }) => {
     }
   };
 
-  // Generate an array of dates for the current month
-  const generateDates = () => {
-    const daysInMonth = new Date(selectedDate.getFullYear(), selectedDate.getMonth() + 1, 0).getDate();
-    return Array.from({ length: daysInMonth }, (_, i) => new Date(selectedDate.getFullYear(), selectedDate.getMonth(), i + 1));
+  // Carousel Navigation
+  const changeDate = (days) => {
+    setSelectedDate(new Date(selectedDate.setDate(selectedDate.getDate() + days)));
   };
 
   return (
     <div className="main-page">
       <h1>Habit Tracker</h1>
 
-      {(
-        <div>
-          {/* Calendar Navigation */}
-          <div className="calendar">
-            <button className="nav-button" onClick={() => setSelectedDate(new Date(selectedDate.setDate(selectedDate.getDate() - 1)))}>&lt;</button>
-            <div className="dates">
-              <div className="date">
-                <span>{selectedDate.getDate()}</span>
-                <span>{selectedDate.toLocaleString('default', { weekday: 'short' })}</span>
-              </div>
+      <div className="carousel">
+        <button className="nav-button" onClick={() => changeDate(-1)}>&lt;</button>
+        <div className="dates">
+          {Array.from({ length: 5 }, (_, i) => 
+            <div key={i} className={`date ${i === 2 ? 'selected' : ''}`}>
+              <span>{new Date(selectedDate.getFullYear(), selectedDate.getMonth(), selectedDate.getDate() + i - 2).getDate().toString().padStart(2, '0')}</span>
+              <span>{new Date(selectedDate.getFullYear(), selectedDate.getMonth(), selectedDate.getDate() + i - 2).toLocaleString('default', { weekday: 'short' })}</span>
             </div>
-            <button className="nav-button" onClick={() => setSelectedDate(new Date(selectedDate.setDate(selectedDate.getDate() + 1)))}>&gt;</button>
-          </div>
+          )}
+        </div>
+        <button className="nav-button" onClick={() => changeDate(1)}>&gt;</button>
+      </div>
 
-          {/* Habit Management Section */}
-          <div className="habits">
-            <h2>Habits for {selectedDate.toLocaleDateString()}</h2>
-            {habits.filter(habit => habit.frequencyDays && habit.frequencyDays.includes(selectedDate.getDay())).map(habit => (
-              <div key={habit._id} className="habit" style={{ backgroundColor: habit.color }}>
-                <input type="checkbox" checked={habit.completedDates.includes(selectedDate.toISOString().split('T')[0])} onChange={() => handleCompleteHabit(habit._id)} />
-                {habit.name}
-              </div>
+      <div className="habits">
+        <h2>Habits for {selectedDate.toLocaleDateString('en-GB')}</h2> {/* Change here */}
+        {habits.filter(habit => habit.frequencyDays.includes(selectedDate.getDay())).map(habit => (
+          <div key={habit._id} className="habit" style={{ backgroundColor: '#4db6ac' }}>
+            <input type="checkbox" checked={habit.completedDates.includes(selectedDate.toISOString().split('T')[0])} />
+              <span>{habit.name}</span>
+          </div>
+        ))}
+
+        <button onClick={() => setShowAddHabitPopup(true)}>Add Habit</button>
+      </div>
+
+      {/* Add Habit Popup */}
+      {showAddHabitPopup && (
+        <div className="popup">
+          <h3>Add New Habit</h3>
+          <input
+            type="text"
+            placeholder="Add Habit"
+            value={newHabit}
+            onChange={(e) => setNewHabit(e.target.value)}
+          />
+          <div className="frequency-selector">
+            {[...Array(7)].map((_, index) => (
+              <label key={index}>
+                <input
+                  type="checkbox"
+                  checked={frequencyDays.includes(index)}
+                  onChange={() => handleFrequencyChange(index)}
+                />
+                {['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'][index]}
+              </label>
             ))}
-
-            <button onClick={() => setShowAddHabitPopup(true)}>Add Habit</button>
-            <button onClick={() => setShowCalendarView(!showCalendarView)}>Toggle Calendar View</button>
           </div>
-
-          {/* Add Habit Popup */}
-          {showAddHabitPopup && (
-            <div className="popup">
-              <h3>Add New Habit</h3>
-              <input
-                type="text"
-                placeholder="Add Habit"
-                value={newHabit}
-                onChange={(e) => setNewHabit(e.target.value)}
-              />
-              <input
-                type="color"
-                value={habitColor}
-                onChange={(e) => setHabitColor(e.target.value)}
-              />
-              <div className="frequency-selector">
-                {[...Array(7)].map((_, index) => (
-                  <label key={index}>
-                    <input
-                      type="checkbox"
-                      checked={frequencyDays.includes(index)}
-                      onChange={() => handleFrequencyChange(index)}
-                    />
-                    {['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'][index]}
-                  </label>
-                ))}
-              </div>
-              <button onClick={handleAddHabit}>Add Habit</button>
-              <button onClick={resetHabitForm}>Cancel</button>
-            </div>
-          )}
-
-          {/* Calendar View */}
-          {showCalendarView && (
-            <div className="calendar-view">
-              {generateDates().map(date => (
-                <div key={date.toString()} className={`calendar-day ${habits.some(habit => habit.frequencyDays && habit.frequencyDays.includes(date.getDay()) && habit.completedDates.includes(date.toISOString().split('T')[0])) ? 'completed' : ''}`}>
-                  {date.getDate()}
-                  {habits.filter(habit => habit.frequencyDays && habit.frequencyDays.includes(date.getDay())).map(habit => (
-                    <div key={habit._id} style={{ backgroundColor: habit.color }} className={`shaded-habit ${habit.completedDates.includes(date.toISOString().split('T')[0]) ? 'completed' : ''}`}>
-                      {habit.name}
-                    </div>
-                  ))}
-                </div>
-              ))}
-            </div>
-          )}
+          <button onClick={handleAddHabit}>Add Habit</button>
+          <button onClick={resetHabitForm}>Cancel</button>
         </div>
       )}
     </div>
